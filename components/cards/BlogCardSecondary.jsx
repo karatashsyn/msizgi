335284fef230cf4e844c22e660380a39fd886bf0
Huseyin Karatas <karatashuseyin2016@gmail.com>
2024-05-20 01:22:48 +0300

    Updates

--- components/cards/BlogCardSecondary.jsx
+++ components/cards/BlogCardSecondary.jsx
@@ -1,19 +1,22 @@
 import { getFormattedDate } from "@/util";
+import Link from "next/link";
 import React from "react";
 
 export default function BlogCardSecondary({ className, blog }) {
   return (
-    <div
-      className={`${className} hover:opacity-65 transition-opacity duration-75 cursor-pointer px-[1rem] max-w-[19rem] min-h-[7rem] py-[1.5rem] border-[1px] border-[#BECBFF]`}
-    >
-      <div className="flex flex-col gap-[0.25rem]">
-        <span className="text-[#062D92] font-light text-[0.85rem]">
-          {getFormattedDate(blog?.date)}
-        </span>
-        <h4 className="text-[#1B262C] font-bold text-[0.9rem]">
-          {blog?.title}
-        </h4>
+    <Link href={`/blog/${blog?.slug}`}>
+      <div
+        className={`${className} hover:opacity-65 transition-opacity duration-75 cursor-pointer px-[1rem] max-w-[19rem] min-h-[7rem] py-[1.5rem] border-[1px] border-[#BECBFF]`}
+      >
+        <div className="flex flex-col gap-[0.25rem]">
+          <span className="text-[#062D92] font-light text-[0.85rem]">
+            {getFormattedDate(blog?.date)}
+          </span>
+          <h4 className="text-[#1B262C] font-bold text-[0.9rem]">
+            {blog?.title}
+          </h4>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
