7e55aa30afc76e91498ebfeedb7896b16a31ee0f
Huseyin Karatas <karatashuseyin2016@gmail.com>
2024-05-20 21:51:48 +0300

    SiteMap and RobotsTxt

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,16 +1,63 @@
 import { getBlogs } from "@/services/blog";
-
+const DOMAIN = "https://www.mehmetsaidizgi.com";
 export default async function sitemap() {
   const blogs = await getBlogs();
   const blogEntries = blogs.map((blog) => ({
-    url: `https://DOMAIN.com/${blog.slug}`,
-    // lastModified: new Date(blog._updatedAt)
-    // changeFreq: "",
-    // priority: 0.8
+    url: `${DOMAIN}/blog/${blog.slug}`,
+    lastModified: new Date(blog.date),
+    priority: 0.8,
+    changeFrequency: "monthly",
   }));
   return [
     {
-      url: "https://DOMAIN.com/",
+      url: `${DOMAIN}`,
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: `${DOMAIN}/iletisim`,
+      lastModified: new Date(),
+      priority: 0.85,
+    },
+    {
+      url: `${DOMAIN}/hakkimizda`,
+      lastModified: new Date(),
+      priority: 0.6,
+    },
+    {
+      url: `${DOMAIN}/blog`,
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: `${DOMAIN}/tedaviler`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${DOMAIN}/en`,
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: `${DOMAIN}/en/iletisim`,
+      lastModified: new Date(),
+      priority: 0.85,
+    },
+    {
+      url: `${DOMAIN}/en/hakkimizda`,
+      lastModified: new Date(),
+      priority: 0.6,
+    },
+    {
+      url: `${DOMAIN}/en/blog`,
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: `${DOMAIN}/en/tedaviler`,
+      lastModified: new Date(),
+      priority: 0.8,
     },
     ...blogEntries,
   ];
