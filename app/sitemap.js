94e0e87c927c2fc243393df74b0ca07cc8e386b5
Huseyin Karatas <karatashuseyin2016@gmail.com>
2024-05-20 22:03:45 +0300

    Updates

--- app/sitemap.js
+++ app/sitemap.js
@@ -22,7 +22,7 @@ export default async function sitemap() {
     {
       url: `${DOMAIN}/hakkimizda`,
       lastModified: new Date(),
-      priority: 0.6,
+      priority: 0.5,
     },
     {
       url: `${DOMAIN}/blog`,
@@ -32,7 +32,32 @@ export default async function sitemap() {
     {
       url: `${DOMAIN}/tedaviler`,
       lastModified: new Date(),
-      priority: 0.8,
+      priority: 0.6,
+    },
+    {
+      url: `${DOMAIN}/hizmetler/implant`,
+      lastModified: new Date(),
+      priority: 0.75,
+    },
+    {
+      url: `${DOMAIN}/hizmetler/pedodonti`,
+      lastModified: new Date(),
+      priority: 0.75,
+    },
+    {
+      url: `${DOMAIN}/hizmetler/dis-beyazlatma`,
+      lastModified: new Date(),
+      priority: 0.75,
+    },
+    {
+      url: `${DOMAIN}/hizmetler/kanal-tedavisi`,
+      lastModified: new Date(),
+      priority: 0.75,
+    },
+    {
+      url: `${DOMAIN}/hizmetler/ortodonti`,
+      lastModified: new Date(),
+      priority: 0.75,
     },
     {
       url: `${DOMAIN}/en`,
@@ -59,6 +84,32 @@ export default async function sitemap() {
       lastModified: new Date(),
       priority: 0.8,
     },
+    {
+      url: `${DOMAIN}/en/services/implant`,
+      lastModified: new Date(),
+      priority: 0.75,
+    },
+    {
+      url: `${DOMAIN}/en/services/pedodontics`,
+      lastModified: new Date(),
+      priority: 0.75,
+    },
+    {
+      url: `${DOMAIN}/en/services/teeth-whitening`,
+      lastModified: new Date(),
+      priority: 0.75,
+    },
+    {
+      url: `${DOMAIN}/en/services/root-canal-treatment`,
+      lastModified: new Date(),
+      priority: 0.75,
+    },
+    {
+      url: `${DOMAIN}/en/services/orthodontics`,
+      lastModified: new Date(),
+      priority: 0.75,
+    },
+
     ...blogEntries,
   ];
 }
