597bb52d12b099295194eed4b81b8d95b7b66935
Huseyin Karatas <karatashuseyin2016@gmail.com>
2024-05-27 12:48:49 +0300

    Updates

--- data.js
+++ data.js
@@ -3,49 +3,50 @@ export const team = [
     name: "Mehmet Said İzgi",
     title: "orthodontist",
     image: "/images/team/said_izgi.webp",
-    experience: 10,
+    // experience is 15 years from current year
+    experience: new Date().getFullYear() - 2009,
   },
   {
     name: "Ceyda Akın",
     title: "dentist",
     image: "/images/team/ceyda_akin.webp",
-    experience: 10,
+    experience: new Date().getFullYear() - 2023,
   },
   {
     name: "Bahar Gürsan",
     title: "dentist",
     image: "/images/team/bahar_gursan.webp",
-    experience: 10,
+    experience: new Date().getFullYear() - 2018,
   },
   {
     name: "Aziz Güvenç",
     title: "dentist",
     image: "/images/team/aziz_guvenc.webp",
-    experience: 10,
+    experience: new Date().getFullYear() - 2019,
   },
 
   {
     name: "Nazlıcan Kaya",
     title: "treatment-analyst",
     image: "/images/team/nazlican_kaya.webp",
-    experience: 10,
+    experience: new Date().getFullYear() - 2018,
   },
   {
     name: "Arzu Parlak",
     title: "treatment-analyst",
     image: "/images/team/arzu_parlak.webp",
-    experience: 10,
+    experience: new Date().getFullYear() - 2021,
   },
   {
     name: "Rümeysa Akıcı",
     title: "treatment-analyst",
     image: "/images/team/rumeysa_akici.webp",
-    experience: 10,
+    experience: new Date().getFullYear() - 2022,
   },
   {
     name: "Eda Ar",
     title: "banko",
     image: "/images/team/eda_ar.webp",
-    experience: 10,
+    experience: new Date().getFullYear() - 2021,
   },
 ];
